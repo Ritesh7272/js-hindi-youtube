f9167af2bdac3902267588ba8ab7b5eedfd370a5
Ritesh7272 <rrai6662@gmail.com>
2024-09-18 13:33:46 +0000

    conversion of datatype

---
+++ 01_basics/02_dataconversion.js
@@ -0,0 +1,28 @@
+// let score = "66zjwk"
+// let score = "ritesh"
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+//  let valueInNumber =Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+ //"33"=>33
+ //"33abf"=>nan
+ //true=>1; false=>0
+
+//  let isLoggedIn= 1;
+
+//  let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+//  console.log(booleanIsLoggedIn);
+
+ //1=> true; 0=>false
+ //""=>false
+ //"ritesh"=> true
+
+
+ let someNumber=45;
+ let StringNumber=String(someNumber)
+ console.log( typeof StringNumber)
