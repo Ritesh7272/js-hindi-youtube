10ed6dcfbb678058ce82a2128acde03319c03f68
Ritesh7272 <rrai6662@gmail.com>
2024-09-18 12:33:28 +0000

    datatypes

--- 01_basics/01_test.js
+++ 01_basics/01_test.js
@@ -1,1 +1,26 @@
 console.log("hitesh")
+//alert(3+3) // ew are using nodejs not brouser
+
+console.log(3+3) 
+console.log("hitesh")
+
+let name ="hitesh"
+let age=40
+let isloggedIn=false
+let state;
+//number=>2 to power 53
+//bigint
+//string=> ""
+
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+// console.log(typeof("hitesh"))
+// console.log(typeof(name))
+// console.log(typeof age)
+// console.log(typeof null)
+// console.log(typeof  undefined)
